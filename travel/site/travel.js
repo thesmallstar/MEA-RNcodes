ac254698b113bbfa25413ed25998c4f8867643a8
thesmallstar <42006277+thesmallstar@users.noreply.github.com>
2019-05-20 19:40:06 +0530

    Update travel.js

--- travel/site/travel.js
+++ travel/site/travel.js
@@ -1,32 +1,35 @@
 var express = require("express");
-
 var app = express();
+
+var handlebars = require("express3-handlebars").create({
+  defaultLayout: "main"
+});
+
+app.engine("handlebars", handlebars.engine);
+app.set("view engine", "handlebars");
 
 app.set("port", process.env.PORT || 3000);
 
 app.get("/", (req, res) => {
-  res.type("text/plane");
-  res.send("Welcome to the travel website");
+  res.render("home");
 });
 
 app.get("/about", (req, res) => {
-  res.type("text/plane");
-  res.send("This is about page");
+  res.render("about");
 });
 
 //404 page
 app.use(function(req, res) {
-  res.type("text/plane");
   res.status(404);
-  res.send("404- page not found");
+  res.render("404");
 });
 
 // 500 page
 app.use(function(err, req, res, next) {
   console.error(err.stack);
-  res.type("text/plane");
+
   res.status(500);
-  res.send("500 error");
+  res.render("500");
 });
 
 app.listen(app.get("port"), function() {
