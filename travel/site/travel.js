d6af1517282d6142392bdcc4d38dd2d50c3efcdc
thesmallstar <42006277+thesmallstar@users.noreply.github.com>
2019-05-21 00:34:23 +0530

    Update travel.js

--- travel/site/travel.js
+++ travel/site/travel.js
@@ -5,17 +5,28 @@ var handlebars = require("express3-handlebars").create({
   defaultLayout: "main"
 });
 
+var fortunes = [
+  "Conquer your fears or they will conquer you.",
+  "Rivers need springs.",
+  "Do not fear what you don't know.",
+  "You will have a pleasant surprise.",
+  "Whenever possible, keep it simple."
+];
+
 app.engine("handlebars", handlebars.engine);
 app.set("view engine", "handlebars");
 
 app.set("port", process.env.PORT || 3000);
+
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
   res.render("home");
 });
 
 app.get("/about", (req, res) => {
-  res.render("about");
+  var RandomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+  res.render("about", { fortune: RandomFortune });
 });
 
 //404 page
