f750ac747df8b6c78e15d6cf58ece2283798d0d8
thesmallstar <manthan.surkar@gmail.com>
2019-05-22 18:32:39 +0530

    addedtests

--- travel/site/travel.js
+++ travel/site/travel.js
@@ -2,23 +2,34 @@ var express = require("express");
 var app = express();
 var fortune = require("./lib/fortune.js");
 
+//handle bars
 var handlebars = require("express3-handlebars").create({
   defaultLayout: "main"
 });
-
 app.engine("handlebars", handlebars.engine);
 app.set("view engine", "handlebars");
 
+//setup ports
 app.set("port", process.env.PORT || 3000);
 
+//setup public dir
 app.use(express.static(__dirname + "/public"));
+
+app.use(function(req, res, next) {
+  res.locals.showTests =
+    app.get("env") !== "production" && req.query.test === "1";
+  next();
+});
 
 app.get("/", (req, res) => {
   res.render("home");
 });
 
 app.get("/about", (req, res) => {
-  res.render("about", { fortunes: fortune.getfortune() });
+  res.render("about", {
+    fortunes: fortune.getfortune(),
+    pageTestScript: "/qa/tests-about.js"
+  });
 });
 
 //404 page
@@ -30,7 +41,6 @@ app.use(function(req, res) {
 // 500 page
 app.use(function(err, req, res, next) {
   console.error(err.stack);
-
   res.status(500);
   res.render("500");
 });
