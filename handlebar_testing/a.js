12667f47acf6acf92acda7d9e7b4d6cfdec73bdc
thesmallstar <manthan.surkar@gmail.com>
2019-05-24 17:53:40 +0530

    testing and scales

---
+++ handlebar_testing/a.js
@@ -0,0 +1,38 @@
+var express = require("express");
+app = express();
+
+//handle bars
+var handlebars = require("express3-handlebars").create({
+  defaultLayout: "main"
+});
+app.engine("handlebars", handlebars.engine);
+app.set("view engine", "handlebars");
+
+//add port to listen
+app.set("port", process.env.PORT || 3000);
+
+// add public direct
+app.use(express.static(__dirname + "/public"));
+
+//MAIN code goes here
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+//404 page
+app.use(function(req, res) {
+  res.status(404);
+  res.render("404");
+});
+
+// 500 page
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500);
+  res.render("500");
+});
+
+app.listen(app.get("port"), function() {
+  console.log("express started " + app.get("port"));
+});
