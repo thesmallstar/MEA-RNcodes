33cd9864150637d9e749c6af94e19c5fe128237b
thesmallstar <manthan.surkar@gmail.com>
2019-07-11 18:31:29 +0530

    ussing mongoose

--- m/index.js
+++ m/index.js
@@ -45,6 +45,42 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
+//User Section //
+
+const User = require("./models/user");
+
+app.get("/user/add", (req, res) => {
+  res.render("adduser");
+});
+
+app.post("/user/add", (req, res) => {
+  const user = new User({
+    _id: mongoose.Types.ObjectId(),
+    name: req.body.name,
+    password: req.body.password
+  });
+
+  user
+    .save()
+    .then(result => {
+      console.log(result);
+    })
+    .catch(err => console.log(err));
+
+  res.send("DOne");
+});
+
+app.get("/user/:id", (req, res, next) => {
+  User.findById(res.params.id)
+    .exec()
+    .then(doc => {
+      console.log(doc);
+    })
+    .catch(err => console.log(err));
+
+  res.send("hi");
+});
+
 //Middlewares and errors
 //404 page
 app.use(function(req, res) {
